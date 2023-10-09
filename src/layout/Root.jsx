e09e1ef7e8639096c88c445ee1e53292cbe5cd7d
Yousufjoy <kanekiken333@gmail.com>
2023-10-09 13:13:44 +0600

    slider,navbar has been added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Root = () => {
   return (
-    <div className=" max-w-7xl mx-auto">
-      <Outlet></Outlet>
-    </div>
+    <>
+      <div className=" max-w-7xl mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../Shared/Navbar/Navbar";
+import Slider from "./Slider/Slider";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <h2 className=" text-center text-6xl py-4">Creating the Best Day Ever</h2>
+      <Slider></Slider>
     </div>
   );
 };
